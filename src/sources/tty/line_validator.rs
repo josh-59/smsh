9eee39463271b21233031aba72aab7fb9ae2bd27
josh-59 <josh.m.timmons@gmail.com>
2024-02-25 10:44:02 -0600

    Moved file back where it belongs

---
+++ src/sources/tty/line_validator.rs
@@ -0,0 +1,37 @@
+use reedline::{Validator, ValidationResult};
+use crate::sources::is_complete;
+
+pub struct SmshLineValidator;
+
+impl Validator for SmshLineValidator {
+    fn validate(&self, line: &str) -> ValidationResult {
+        if ! is_complete(line) {    // Every line/line+block must be complete
+            ValidationResult::Incomplete
+        } else if is_shell_construct(line) {
+            if contains_finished_block(line) {
+                ValidationResult::Complete
+            } else {
+                ValidationResult::Incomplete
+            }
+        } else {
+            ValidationResult::Complete
+        }
+    }
+}
+
+// A block is complete if it ends with an empty line
+fn contains_finished_block(line: &str) -> bool {
+    line.ends_with("\n\n") ||
+    line.ends_with("\n \n") ||
+    line.ends_with("\n  \n") ||
+    line.ends_with("\n   \n")
+}
+
+// TODO: Handle leading whitespace
+fn is_shell_construct(line: &str) -> bool {
+    line.starts_with("if") ||
+    line.starts_with("for") ||
+    line.starts_with("while") ||
+    line.starts_with("case") ||
+    line.starts_with("fn")
+}
