3f1431d180a99e06d7e13d23b13bb73041e1cd97
josh-59 <josh.m.timmons@gmail.com>
2024-03-02 09:26:22 -0600

    Removed shell_noexec, for now

--- src/shell/state.rs
+++ src/shell/state.rs
@@ -1,9 +1,6 @@
 pub struct State {
     interactive: bool,
 
-    // True if shell is to carry out parsing only
-    no_exec: bool,
-
     // Each builtin and each external command sets (resets) this.
     // Shell constructs do not affect this.
     pub rv: i32,
@@ -13,16 +10,11 @@ impl State {
     pub fn new() -> Self {
         State {
             interactive: true,
-            no_exec: false,
             rv: 0,
         }
     }
 
     pub fn is_interactive(&self) -> bool {
         self.interactive
-    }
-
-    pub fn no_exec(&self) -> bool {
-        self.no_exec
     }
 }
